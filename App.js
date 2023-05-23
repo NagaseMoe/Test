9484c7fa10b4f6b7f7b743adbf3d61b00deb60b8
NagaseMoe <22aw0126@jec.ac.jp>
2023-05-23 12:21:25 +0900

    タブアイコンUI

--- App.js
+++ App.js
@@ -4,31 +4,36 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './pages/Home';
 import TodoScreen from './pages/Todo';
 import StoreList from './pages/StoreList';
-
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Home!</Text>
-//     </View>
-//   );
-// }
-
-// function SettingsScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Settings!</Text>
-//     </View>
-//   );
-// }
+// import { Ionicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="比較計算" component={HomeScreen} />
-      <Tab.Screen name="Todoリスト" component={TodoScreen} />
-      <Tab.Screen name="店舗メモ" component={StoreList} />
+      <Tab.Screen name="比較計算" component={HomeScreen} 
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Entypo name="calculator" size={size} color={color} />
+        ),
+      }}
+      />
+      <Tab.Screen name="Todoリスト" component={TodoScreen} 
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Feather name="file-text" size={size} color={color} />
+        ),
+      }}
+      />
+      <Tab.Screen name="店舗メモ" component={StoreList} 
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome5 name="store" size={size} color={color} />
+        ),
+      }}/>
     </Tab.Navigator>
   );
 }
