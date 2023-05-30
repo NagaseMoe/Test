3fcd30a5737f1b112041703244a99749e04701c2
NagaseMoe <22aw0126@jec.ac.jp>
2023-05-30 12:56:32 +0900

    比較計算機能_if文変更前

--- pages/Home.js
+++ pages/Home.js
@@ -1,12 +1,68 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text } from 'react-native';
 
-function HomeScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>計算機</Text>
+const Calculator = () => {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  // const [number3, setNumber3] = useState('');
+  // const [number4, setNumber4] = useState('');
+  const [operator, setOperator] = useState('');
+  const [result, setResult] = useState('');
+
+  const calculate = () => {
+    const num1 = parseFloat(number1);
+    const num2 = parseFloat(number2);
+    // const num3 = parseFloat(number3);
+    // const num4 = parseFloat(number4);
+
+    if (isNaN(num1) || isNaN(num2)) {
+      setResult('値が入力されていません');
+      return;
+    }
+
+    let calculatedResult;
+    // switch (operator) {
+    //   case '<':
+    //     calculatedResult = num1 < num2;
+    //     break;
+    //   case '>':
+    //     calculatedResult = num1 > num2;
+    //     break;
+    //   case '=':
+    //     calculatedResult = num1 === num2;
+    //     break;
+    //   default:
+    //     setResult('Invalid operator');
+    //     return;
+    // }
+
+    setResult(calculatedResult ? 'True' : 'False');
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="価格A"
+        value={number1}
+        onChangeText={text => setNumber1(text)}
+        keyboardType="numeric"
+      />
+      <TextInput
+        placeholder='価格B'
+        value={number2}
+        onChangeText={text => setNumber2(text)}
+        keyboardType='numeric'
+      />
+      <TextInput
+        placeholder="Enter operator (<, >, =)"
+        value={operator}
+        onChangeText={text => setOperator(text)}
+        maxLength={1}
+      />
+      <Button title="比較する" onPress={calculate} />
+      {result !== '' && <Text>Result: {result}</Text>}
     </View>
-    )
-}
+  );
+};
 
-export default HomeScreen;
+export default Calculator;
