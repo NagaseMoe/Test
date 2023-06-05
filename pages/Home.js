5a256dffde659c79f4d163612293d8bf15f7f0b7
NagaseMoe <22aw0126@jec.ac.jp>
2023-06-05 15:37:11 +0900

    「比較する」ボタンのカラーチェンジ

--- pages/Home.js
+++ pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text, ScrollView } from 'react-native';
+import { View, TextInput, TouchableOpacity, Text, ScrollView } from 'react-native';
 
 const Calculator = () => {
   const [number1, setNumber1] = useState('');
@@ -8,24 +8,33 @@ const Calculator = () => {
   const [number4, setNumber4] = useState('');
   const [result, setResult] = useState('');
 
-  const containerStyle = {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-  }
-  const containerStyle1 = {
-    width: '45%',
-    margin: 5,
-    padding: 10,
-    borderWidth: 1,
-    borderStyle: 'style',
-    borderRadius: 5
-  }
-  const frame = {
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderRadius: 5,
-    height: 50
+  const styles ={
+    containerStyle : {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    containerStyle1 : {
+      width: '45%',
+      margin: 5,
+      padding: 10,
+      borderWidth: 1,
+      borderStyle: 'style',
+      borderRadius: 15
+    },
+    frame : {
+      borderWidth: 1,
+      borderStyle: 'solid',
+      borderRadius: 15,
+      height: 50
+    },
+    calculatorButton : {
+      backgroundColor: '#FBB03A',
+      width: '80%',
+      margin: 15,
+      borderRadius: 15,
+      textAlign: 'center'
+    }
   }
 
   const calculate = () => {
@@ -48,39 +57,41 @@ const Calculator = () => {
 
   return (
     <>
-    <ScrollView scrollEnabled={false}>
-      <View style={containerStyle}>
-      <View style={containerStyle1}>
+    <ScrollView scrollEnabled={false} containerStyle={{display: 'inlineBlock'}}>
+      <View style={styles.containerStyle}>
+      <View style={styles.containerStyle1}>
       <Text>価格A</Text>
-      <TextInput style={frame}
+      <TextInput style={styles.frame}
         placeholder = '値段を入力'
         value={number1}
         onChangeText={text => setNumber1(text)}
       />
       <Text>量（g、ml、個）</Text>
-      <TextInput style={frame}
+      <TextInput style={styles.frame}
         placeholder = '量を入力'
         value={number2}
         onChangeText={text => setNumber2(text)}
       />
       </View>
-      <View style={containerStyle1}>
+      <View style={styles.containerStyle1}>
       <Text>価格B</Text>
-      <TextInput style={frame}
+      <TextInput style={styles.frame}
         placeholder = '値段を入力'
         value={number3}
         onChangeText={text => setNumber3(text)}
       />
       <Text>量（g、ml、個）</Text>
-      <TextInput style={frame}
+      <TextInput style={styles.frame}
         placeholder = '量を入力'
         value={number4}
         onChangeText={text => setNumber4(text)}
       />
       </View>
       </View>
-      <Button title="比較する" onPress={calculate} />
-      {result !== '' && <Text>{result}</Text>}
+      <TouchableOpacity style={styles.calculatorButton} onPress={calculate}>
+        <Text style={{ color: 'white', padding: 15 , textAlign: 'center'}}>比較する</Text>
+      </TouchableOpacity>
+        {result !== '' && <Text>{result}</Text>}
     </ScrollView>
     </>
   );
