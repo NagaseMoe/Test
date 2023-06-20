336c229a8003f76646f1ce0e304d639b8b3d24b9
tas321 <taskwork321@gmail.com>
2023-06-20 09:52:08 +0900

    0620

--- pages/Todo.js
+++ pages/Todo.js
@@ -1,12 +1,180 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Text,
+  Button,
+  Keyboard,
+} from "react-native";
 
 function TodoScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Todoリスト</Text>
     </View>
-    )
+
+  );
 }
+
+// 追加部分
+export default function App() {
+  const [inputFields, setInputFields] = useState([
+    { id: Date.now(), value: "", checked: false },
+  ]);
+  const [keyboardStatus, setKeyboardStatus] = useState("");
+
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener("keyboardDidShow", () => {
+      setKeyboardStatus("Keyboard Shown");
+    });
+    const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {
+      setKeyboardStatus("Keyboard Hidden");
+    });
+
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
+
+  const addInput = () => {
+    setInputFields([
+      ...inputFields,
+      { id: Date.now(), value: "", checked: false },
+    ]);
+  };
+
+  const handleInputChange = (text, id) => {
+    const updatedInputFields = inputFields.map((field) => {
+      if (field.id === id) {
+        return { ...field, value: text };
+      }
+      return field;
+    });
+    setInputFields(updatedInputFields);
+  };
+
+  const handleCheckboxChange = (id) => {
+    const updatedInputFields = inputFields.map((field) => {
+      if (field.id === id) {
+        return { ...field, checked: !field.checked };
+      }
+      return field;
+    });
+    setInputFields(updatedInputFields);
+  };
+
+  const removeInput = (id) => {
+    const updatedInputFields = inputFields.filter((field) => field.id !== id);
+    setInputFields(updatedInputFields);
+  };
+
+  const removeCheckedInputs = () => {
+    const updatedInputFields = inputFields.filter((field) => !field.checked);
+    setInputFields(updatedInputFields);
+  };
+
+  return (
+    <View style={styles.container}>
+      {inputFields.map((inputField) => (
+        <View key={inputField.id} style={styles.inputContainer}>
+          <TouchableOpacity onPress={() => handleCheckboxChange(inputField.id)}>
+            <View
+              style={[
+                styles.checkbox,
+                inputField.checked
+                  ? styles.checkedCheckbox
+                  : styles.uncheckedCheckbox,
+              ]}
+            >
+              {inputField.checked && <Text style={styles.checkboxText}>✓</Text>}
+            </View>
+          </TouchableOpacity>
+          <TextInput
+            style={styles.input}
+            value={inputField.value}
+            onChangeText={(text) => handleInputChange(text, inputField.id)}
+            autoFocus={true}
+          />
+        </View>
+      ))}
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={addInput}>
+          <Text style={styles.buttonText}>プラス</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={removeCheckedInputs}>
+          <Text style={styles.buttonText}>削除</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.status}>{keyboardStatus}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 8,
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    borderWidth: 2,
+    borderColor: "gray",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  checkedCheckbox: {
+    backgroundColor: "green",
+  },
+  uncheckedCheckbox: {
+    backgroundColor: "transparent",
+  },
+  checkboxText: {
+    color: "white",
+  },
+  input: {
+    flex: 1,
+    height: 60,
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderColor: "gray",
+    paddingHorizontal: 8,
+    paddingVertical: 0,
+    alignSelf: "stretch",
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#000",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 4,
+    marginRight: 8, // 追加
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  status: {
+    marginTop: 16,
+    textAlign: "center",
+  },
+});
 
 export default TodoScreen;
