cade3579fd060cfa30a9f62f363e0af9a9e7d691
NagaseMoe <22aw0126@jec.ac.jp>
2023-07-11 09:38:23 +0900

    コンポーネントが増えてくれません泣

--- pages/Details.js
+++ pages/Details.js
@@ -1,169 +1,154 @@
 import * as React from "react";
-import { View, Text, StyleSheet, TextInput, ScrollView, SafeAreaView, TouchableOpacity} from 'react-native';
-import RNPickerSelect from 'react-native-picker-select';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+  ScrollView,
+  SafeAreaView,
+  StyleSheet,
+  Image
+} from "react-native";
 import { useState, useEffect } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const DetailsScreen = () => {
-  //以下はデフォルトで表示されているコンポーネント
-  const [selectedValue, setSelectedValue] = useState('');
-  const [components, setComponents] = useState([
-    <View key={0} style={styles.containerStyle}>
-      <View style={[styles.pulldownStyle, { width: '30%' }]}>
-        <Text style={styles.textStyle}>店舗名</Text>
-        <RNPickerSelect
-          style={styles.textInputStyle}
-          onValueChange={(itemValue) => setSelectedValue(itemValue)}
-          items={[
-            { label: 'オプション1', value: 'option1' },
-            { label: 'オプション2', value: 'option2' },
-            { label: 'オプション3', value: 'option3' },
-          ]}
-        />
-      </View>
-      <View style={styles.textflex}>
-        <View style={styles.pulldownStyle}>
-          <Text style={styles.textStyle}>量</Text>
-          <TextInput
-            style={styles.textInputStyle}
-            placeholder="0"
-            keyboardType="numeric"
-          />
-        </View>
-        <Text>あたり</Text>
-      </View>
-      <View style={styles.textflex}>
-        <View style={styles.pulldownStyle}>
-          <Text style={styles.textStyle}>値段</Text>
-          <TextInput
-            style={styles.textInputStyle}
-            placeholder="0"
-            keyboardType="numeric"
-          />
-        </View>
-        <Text>円</Text>
-      </View>
-    </View>
-  ]);
+function DetailsScreen () {
+    const [inputDetails, setInputDetails] = useState([
+        {id: Date.now(), value: ""},
+    ]);
+    const [textValues, setTextValues] = useState([""]);
 
-  //追加ボタンを押した後の処理
-  //ボタンを押したら以下のコンポーネントが追加されていくよ
-  const addComponent = () => {
-    const newComponent = (
-      <View key={components.length} style={styles.containerStyle}>
-        <View style={[styles.pulldownStyle, { width: '30%' }]}>
-          <Text style={styles.textStyle}>店舗名</Text>
-          <RNPickerSelect
-            style={styles.textInputStyle}
-            onValueChange={(itemValue) => setSelectedValue(itemValue)}
-            items={[
-              { label: 'オプション1', value: 'option1' },
-              { label: 'オプション2', value: 'option2' },
-              { label: 'オプション3', value: 'option3' },
-            ]}
-          />
-        </View>
-      <View style={styles.textflex}>
-        <View style={styles.pulldownStyle}>
-          <Text style={styles.textStyle}>量</Text>
-          <TextInput
-            style={styles.textInputStyle}
-            placeholder="0"
-            keyboardType="numeric"
-          />
-        </View>
-        <Text>あたり</Text>
-      </View>
-      <View style={styles.textflex}>
-        <View style={styles.pulldownStyle}>
-          <Text style={styles.textStyle}>値段</Text>
-          <TextInput
-            style={styles.textInputStyle}
-            placeholder="0"
-            keyboardType="numeric"
-          />
-        </View>
-        <Text>円</Text>
-      </View>
-    </View>
-  );
+    const addComponent = () => {
+        setInputDetails([
+            ...inputDetails,
+            { id: Date.now(), value: ""},
+        ]);
+    };
 
-  setComponents([...components, newComponent]);
-};
+    const handleComponentChange = (text, id) => {
+        const updateDetails = inputDetails.map((Details) => {
+            if (Details.id === id) {
+                return {...Details, value: text};
+            }
+            return Details;
+        });
+        setInputDetails(updateDetails);
+    };
 
-  return (
-    <View style={styles.container}>
-      <SafeAreaView style={{ backgroundColor: '#fff' }}>
-        <ScrollView scrollEnabled={true}>
-          {components.map((component, index) => (
-            <React.Fragment key={index}>{component}</React.Fragment>
-          ))}
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button} onPress={addComponent}>
-              <Text style={styles.buttonText}>+</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </View>
-  );
-};
+    return(
+        <View style={styles.container}>
+            <SafeAreaView style={{backgroundColor:"#fff"}}>
+                <ScrollView scrollEnabled={true}>
+                    <View style={styles.containerStyle}>
+                        <View style={[styles.pulldownStyle, { width: '30%' }]}>
+                            <Text style={styles.textStyle}>店舗名</Text>
+                            <TextInput
+                                style={styles.textInputStyle}
+                                placeholder="店舗名"
+                                value={inputDetails.value}
+                                onChangeText={(text) => handleComponentChange(text, inputDetails.id)}
+                            />
+                        </View>
+                        <View style={styles.textflex}>
+                            <View style={styles.pulldownStyle}>
+                                <Text style={styles.textStyle}>量</Text>
+                                <TextInput
+                                style={styles.textInputStyle}
+                                placeholder="0"
+                                keyboardType="numeric"
+                                value={inputDetails.value}
+                                onChangeText={(text) => handleComponentChange(text, inputDetails.id)}
+                            />
+                            </View>
+                                <Text>あたり</Text>
+                        </View>
+                        <View style={styles.textflex}>
+                            <View style={styles.pulldownStyle}>
+                                <Text style={styles.textStyle}>値段</Text>
+                                <TextInput
+                                    style={styles.textInputStyle}
+                                    placeholder="0"
+                                    keyboardType="numeric"
+                                    value={inputDetails.value}
+                                    onChangeText={(text) => handleComponentChange(text, inputDetails.id)}
+                                />
+                            </View>
+                                <Text>円</Text>
+                        </View>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity style={styles.button} onPress={addComponent}>
+                            <Text style={styles.buttonText}>+</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </View>
+);
+}
 
 const styles = StyleSheet.create ({
-  container: {
+    container: {
     flex: 1,
     backgroundColor: "#fff",
-  },
-  containerStyle: {
+    },
+    containerStyle: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
     marginTop: 20,
-  },
-  textflex: {
+    },
+    containerStyle1: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    },
+    textflex: {
     flexDirection: 'row',
     alignItems: 'center',
-  },
-  pulldownStyle: {
+    },
+    pulldownStyle: {
     borderStyle: 'solid',
     borderRadius: 8,
     borderWidth: 1,
     borderColor: '#f0f0f0'
-  },
-  textStyle: {
+    },
+    textStyle: {
     backgroundColor: '#FBB03A',
     color: 'white',
     fontSize: 18,
     textAlign: 'center',
-  },
-  textInputStyle: {
+    },
+    textInputStyle: {
     fontSize: 18,
     padding: 10,
-  },
-  buttonContainer: {
+    },
+    buttonContainer: {
     marginTop: 16,
     flexDirection: "row",
     justifyContent: "flex-start",
     alignItems: "center",
     shadowColor: "#000",
-  shadowOffset: {
+    shadowOffset: {
     width: 0,
     height: 4,
-  },
-  shadowOpacity: 0.2,
-  shadowRadius: 6,
-  elevation: 5,
-  },
-  button: {
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    elevation: 5,
+    },
+    button: {
     backgroundColor: "#fff",
     paddingHorizontal: 16,
     paddingVertical: 4,
     borderRadius: 50,
     marginRight: 8, // 追加
-  },
-  buttonText: {
+    },
+    buttonText: {
     color: "#FBB03A",
     fontSize: 40,
-  },
-});
+    },
+    });
 
-export default DetailsScreen;
+    export default DetailsScreen;
