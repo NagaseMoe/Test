263262b66ec34b42dd51cd1ae34895b8acd22b25
NagaseMoe <22aw0126@jec.ac.jp>
2023-07-11 11:58:06 +0900

    DetailsページのAsyncStorage実装済

--- pages/Details.js
+++ pages/Details.js
@@ -4,90 +4,135 @@ import {
   View,
   TouchableOpacity,
   TextInput,
-  Keyboard,
   ScrollView,
   SafeAreaView,
   StyleSheet,
-  Image
 } from "react-native";
 import { useState, useEffect } from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-function DetailsScreen () {
-    const [inputDetails, setInputDetails] = useState([
-        {id: Date.now(), value: ""},
+function DetailsScreen() {
+  const [inputDetails, setInputDetails] = useState([
+    { id: Date.now(), name: "", quantity: "", price: "" },
+  ]);
+
+  const addComponent = () => {
+    setInputDetails([
+      ...inputDetails,
+      { id: Date.now(), name: "", quantity: "", price: "" },
     ]);
-    const [textValues, setTextValues] = useState([""]);
+  };
 
-    const addComponent = () => {
-        setInputDetails([
-            ...inputDetails,
-            { id: Date.now(), value: ""},
-        ]);
-    };
+  const handleNameChange = (text, id) => {
+    const updatedDetails = inputDetails.map((details) => {
+      if (details.id === id) {
+        return { ...details, name: text };
+      }
+      return details;
+    });
+    setInputDetails(updatedDetails);
+  };
 
-    const handleComponentChange = (text, id) => {
-        const updateDetails = inputDetails.map((Details) => {
-            if (Details.id === id) {
-                return {...Details, value: text};
-            }
-            return Details;
-        });
-        setInputDetails(updateDetails);
-    };
+  const handleQuantityChange = (text, id) => {
+    const updatedDetails = inputDetails.map((details) => {
+      if (details.id === id) {
+        return { ...details, quantity: text };
+      }
+      return details;
+    });
+    setInputDetails(updatedDetails);
+  };
 
-    return(
-        <View style={styles.container}>
-            <SafeAreaView style={{backgroundColor:"#fff"}}>
-                <ScrollView scrollEnabled={true}>
-                    {inputDetails.map((inputDetails) => (
-                        <View key={inputDetails.id} style={styles.containerStyle}>
-                        <View style={[styles.pulldownStyle, { width: '30%' }]}>
-                            <Text style={styles.textStyle}>店舗名</Text>
-                            <TextInput
-                                style={styles.textInputStyle}
-                                placeholder="店舗名"
-                                value={inputDetails.value}
-                                onChangeText={(text) => handleComponentChange(text, inputDetails.id)}
-                            />
-                        </View>
-                        <View style={styles.textflex}>
-                            <View style={styles.pulldownStyle}>
-                                <Text style={styles.textStyle}>量</Text>
-                                <TextInput
-                                style={styles.textInputStyle}
-                                placeholder="0"
-                                keyboardType="numeric"
-                                value={inputDetails.value}
-                                onChangeText={(text) => handleComponentChange(text, inputDetails.id)}
-                            />
-                            </View>
-                                <Text>あたり</Text>
-                        </View>
-                        <View style={styles.textflex}>
-                            <View style={styles.pulldownStyle}>
-                                <Text style={styles.textStyle}>値段</Text>
-                                <TextInput
-                                    style={styles.textInputStyle}
-                                    placeholder="0"
-                                    keyboardType="numeric"
-                                    value={inputDetails.value}
-                                    onChangeText={(text) => handleComponentChange(text, inputDetails.id)}
-                                />
-                            </View>
-                                <Text>円</Text>
-                        </View>
-                    </View>
-                    ))}
-                    <View style={styles.buttonContainer}>
-                        <TouchableOpacity style={styles.button} onPress={addComponent}>
-                            <Text style={styles.buttonText}>+</Text>
-                        </TouchableOpacity>
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
-        </View>
-);
+  const handlePriceChange = (text, id) => {
+    const updatedDetails = inputDetails.map((details) => {
+      if (details.id === id) {
+        return { ...details, price: text };
+      }
+      return details;
+    });
+    setInputDetails(updatedDetails);
+  };
+
+  const saveDataToAsyncStorage = async () => {
+    try {
+      await AsyncStorage.setItem('inputDetails', JSON.stringify(inputDetails));
+      console.log('データが正常に保存されました');
+    } catch (error) {
+      console.log('データの保存中にエラーが発生しました:', error);
+    }
+  };
+
+  const loadDataFromAsyncStorage = async () => {
+    try {
+      const data = await AsyncStorage.getItem('inputDetails');
+      if (data) {
+        setInputDetails(JSON.parse(data));
+      }
+    } catch (error) {
+      console.log('データの取得中にエラーが発生しました:', error);
+    }
+  };
+
+  useEffect(() => {
+    loadDataFromAsyncStorage();
+  }, []);
+
+  useEffect(() => {
+    saveDataToAsyncStorage();
+  }, [inputDetails]);
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={{ backgroundColor: "#fff" }}>
+        <ScrollView scrollEnabled={true}>
+          {inputDetails.map((details) => (
+            <View key={details.id} style={styles.containerStyle}>
+              <View style={[styles.pulldownStyle, { width: '30%' }]}>
+                <Text style={styles.textStyle}>店舗名</Text>
+                <TextInput
+                  style={styles.textInputStyle}
+                  placeholder="店舗名"
+                  value={details.name}
+                  onChangeText={(text) => handleNameChange(text, details.id)}
+                />
+              </View>
+              <View style={styles.textflex}>
+                <View style={styles.pulldownStyle}>
+                  <Text style={styles.textStyle}>量</Text>
+                  <TextInput
+                    style={styles.textInputStyle}
+                    placeholder="0"
+                    keyboardType="numeric"
+                    value={details.quantity}
+                    onChangeText={(text) => handleQuantityChange(text, details.id)}
+                  />
+                </View>
+                <Text>あたり</Text>
+              </View>
+              <View style={styles.textflex}>
+                <View style={styles.pulldownStyle}>
+                  <Text style={styles.textStyle}>値段</Text>
+                  <TextInput
+                    style={styles.textInputStyle}
+                    placeholder="0"
+                    keyboardType="numeric"
+                    value={details.price}
+                    onChangeText={(text) => handlePriceChange(text, details.id)}
+                  />
+                </View>
+                <Text>円</Text>
+              </View>
+            </View>
+          ))}
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={addComponent}>
+              <Text style={styles.buttonText}>+</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create ({
@@ -151,6 +196,7 @@ const styles = StyleSheet.create ({
     color: "#FBB03A",
     fontSize: 40,
     },
-    });
+});
+    
 
-    export default DetailsScreen;
+export default DetailsScreen;
